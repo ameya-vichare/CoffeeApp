cac9cd8cec83b81a478183ebf89d32c853d3ab21
Ameya Vichare <ameyavi@gmail.com>
2025-09-13 14:48:59 +0530

    Initial Commit

---
+++ Coffee App/Coffee_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_AppApp.swift
+//  Coffee App
+//
+//  Created by Ameya on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffee App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Coffee App
+//
+//  Created by Ameya on 13/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
