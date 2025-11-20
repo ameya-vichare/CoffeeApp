82e0756563efadfee1c5b8263d53d5a9ff11a501
Ameya Vichare <ameyavi@gmail.com>
2025-11-20 20:24:58 +0530

    DefaultMenuModifierBottomSheetViewModelTests addition

---
+++ CoffeeModule/Tests/CoffeeModuleTests/MenuBottomSheet/ViewModelTests/DefaultMenuModifierBottomSheetViewModelTests.swift
@@ -0,0 +1,25 @@
+//
+//  DefaultMenuModifierBottomSheetViewModelTests.swift
+//  CoffeeModule
+//
+//  Created by Ameya on 20/11/25.
+//
+
+import XCTest
+@testable import CoffeeModule
+
+final class DefaultMenuModifierBottomSheetViewModelTests: XCTestCase {
+
+    var sut: DefaultMenuModifierBottomSheetViewModel!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+        try super.tearDownWithError()
+    }
+
+}
