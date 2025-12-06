179b92119914c1dbd1301d4ad63c6d79b3cfafa5
Ameya Vichare <ameyavi@gmail.com>
2025-12-06 18:41:43 +0530

    UserSession keys changes

--- Modules/Core/AppModels/Sources/AppModels/UserSession/UserSession.swift
+++ Modules/Core/AppModels/Sources/AppModels/UserSession/UserSession.swift
@@ -10,6 +10,12 @@ public struct UserSession: Codable {
     let userName: String
     let token: String
     
+    enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case userName = "user_name"
+        case token
+    }
+    
     public init(userId: String, userName: String, token: String) {
         self.userId = userId
         self.userName = userName
